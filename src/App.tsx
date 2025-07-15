350dee6ae9b79f6e6bbdc0847c89ea94a355b9ad
usamamukarram-arbisoft <usama.mukarram@arbisoft.com>
2025-07-15 16:18:10 +0500

    fix: navbar issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import { useLocation } from "react-router-dom";
 
 import TopNavbar from "./Components/Navbar/TopNavbar";
 import Routing from "./Components/RountingComponent/Routing";
+import { ROUTES } from "./Utility/CommonContant";
 
 function App() {
   const location = useLocation().pathname;
 
   return (
     <div>
-      {location !== "/login" && <TopNavbar />}
+      {location !== ROUTES.login && location !== ROUTES.logout && <TopNavbar />}
       <Routing />
     </div>
   );
